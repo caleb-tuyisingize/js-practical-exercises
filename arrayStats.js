3794a6e5a03514b7bdd69d522213cd99619154d5
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-06-17 12:41:14 +0200

    arrayStats exercise completed

--- arrayStats.js
+++ arrayStats.js
@@ -2,7 +2,7 @@ const array = [1, 2, 3, 4, 5];
 const arrayStats = ()=>{
     // const sum = array.reduce((acc, num) => acc + num, 0);
     const sum = array.reduce((acc,num) => acc+num, 0);
-    const average = sum / array.length;
+    const average =parseFloat(sum / array.length).toFixed(2);
     const max = Math.max(...array);
     const min = Math.min(...array);
 
