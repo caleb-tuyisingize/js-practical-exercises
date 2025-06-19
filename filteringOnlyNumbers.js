074981aab690f3a1f3ba6a75a8a58b47e5840ffd
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-06-19 11:42:44 +0200

    Jokes with udacity

--- filteringOnlyNumbers.js
+++ filteringOnlyNumbers.js
@@ -1,5 +1,13 @@
-function filter_list(l) {
-    return l.filter(a => typeof a === 'number');
+// function
+//  filter_list(l) {
+//     return l.filter(a => typeof a === 'number');
 
-}
-console.log(filter_list([1,2,'aasf','1','123',123])); // Output: [1, 2, 123]
+// }
+// console.log(filter_list([1,2,'aasf','1','123',123])); // Output: [1, 2, 123]
+
+// console.log("The man whispered \"\n\tPlease speak to me\"");
+// console.log("The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.");
+// console.log("Green">"green");
+
+var joke = "Why couldn't the shoes go out and play? \nThey were all \"tied\" up!";
+console.log(joke);
