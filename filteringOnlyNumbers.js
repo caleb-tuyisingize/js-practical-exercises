f9bacfcdfa5d5d5b92fe58ffc27b9b3563bf51a4
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-06-19 14:56:11 +0200

    logical opperators with udacity

--- filteringOnlyNumbers.js
+++ filteringOnlyNumbers.js
@@ -49,19 +49,53 @@
 //     console.log("NaN");
 // }
 
-var musicians = 1;
+// var musicians = 1;
 
-if(musicians <= 0){
-    console.log("not a group");
-}else if(musicians == 1){
-    console.log("solo");
-}else if(musicians == 2){
-    console.log("duet");
-}else if(musicians == 3){
-    console.log("trio");
-}else if(musicians == 4){
-    console.log("quartet");
-}else if(musicians > 4){
-    console.log("this is a large group");
+// if(musicians <= 0){
+//     console.log("not a group");
+// }else if(musicians == 1){
+//     console.log("solo");
+// }else if(musicians == 2){
+//     console.log("duet");
+// }else if(musicians == 3){
+//     console.log("trio");
+// }else if(musicians == 4){
+//     console.log("quartet");
+// }else if(musicians > 4){
+//     console.log("this is a large group");
+// }
+
+
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `balance`, `checkBalance`, `isActive`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should produce the expected output
+ * 4. Your code should not be empty
+ * 5. BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED. 
+ */
+ 
+// Change these to test different scenarios:
+let balance = 325.00;
+let isActive = true;
+let checkBalance = true;
+
+if (checkBalance) {
+  if (isActive && balance > 0) {
+    console.log("Your balance is $" + balance.toFixed(2) + ".");
+  } else if (!isActive) {
+    console.log("Your account is no longer active.");
+  } else if (isActive && balance === 0) {
+    console.log("Your account is empty.");
+  } else if (isActive && balance < 0) {
+    console.log("Your balance is negative. Please contact bank.");
+  }
+} else {
+  console.log("Thank you. Have a nice day!");
 }
+
 
