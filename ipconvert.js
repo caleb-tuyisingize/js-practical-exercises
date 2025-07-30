48d27876213c39ef84a65d351b41bd42df57b619
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-07-30 09:16:45 +0200

    The kata for counting the ip adresses another 3rd method

--- ipconvert.js
+++ ipconvert.js
@@ -1,7 +1,20 @@
 function ipsBetween(start, end){
-  const ipConverter = (ip)=> ip.split(".").map(x=> parseInt(x)).reduce((num , conv) => num * 256+conv);
+    //--WAY ONE USED
+//   const ipConverter = (ip)=> ip.split(".").map(x=> parseInt(x)).reduce((num , conv) => num * 256+conv);
+    //-- WAY I USED
 //   const ipConverter = (ip)=> ip.split(".").reduce((num , conv) => num * 256 + +conv,0);
-  return ipConverter(end) - ipConverter(start);
+    //WAY THREE BY LOOP
+
+    let ip1 = start.split(".");
+    let ip2 = end.split(".");
+    let result = 0;
+    for(let i = 0; i < 4; i++){
+        result += (ip2[i] - ip1[i]) * 256 **(3-i);
+    }
+
+
+//   return ipConverter(end) - ipConverter(start);
+return result;
 }
  console.log(ipsBetween("150.0.0.0", "150.0.0.1"));
   console.log(ipsBetween("10.0.0.0", "10.0.0.50"));
