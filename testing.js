b1a793ae3bebf7bc735ab189994c64f74e70c740
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-03 21:26:02 +0200

    Testing on generator function 2nd try

--- testing.js
+++ testing.js
@@ -8,3 +8,15 @@ const getNames = generatorFunction();
 console.log(getNames.next());
 console.log(getNames.next());
 console.log(getNames.next());
+function* countToThree() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const counter = countToThree();
+
+console.log(counter.next()); // { value: 1, done: false }
+console.log(counter.next()); // { value: 2, done: false }
+console.log(counter.next()); // { value: 3, done: false }
+console.log(counter.next()); // { value: undefined, done: true }
