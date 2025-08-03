2d8ce1cb0a00fbc5421c7afc9a2bfde2fc393700
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-03 21:47:31 +0200

    Testing on generator function sets try

--- testing.js
+++ testing.js
@@ -20,3 +20,11 @@ console.log(counter.next()); // { value: 1, done: false }
 console.log(counter.next()); // { value: 2, done: false }
 console.log(counter.next()); // { value: 3, done: false }
 console.log(counter.next()); // { value: undefined, done: true }
+
+const oldArray = [1,1,2,3,7,5,3,4,5,3,2,9,9,0,3];
+
+const newArray = new Set(oldArray);
+const building = oldArray.map(e => e*e);
+console.log(newArray);
+console.log(oldArray);
+console.log(building);
