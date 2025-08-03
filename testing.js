a01cdf070791ad472bc79059cb284d3397903734
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-03 20:16:08 +0200

    Testing on generator function

--- testing.js
+++ testing.js
@@ -1,2 +1,10 @@
-console.log(user); // ❌ ReferenceError: Cannot access 'user' before initialization
 let user = 'Caleb';
+console.log(user); // ❌ ReferenceError: Cannot access 'user' before initialization
+function* generatorFunction() {
+  yield 'Hello';
+  yield 'World';
+}
+const getNames = generatorFunction();
+console.log(getNames.next());
+console.log(getNames.next());
+console.log(getNames.next());
