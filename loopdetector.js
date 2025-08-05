e96f4f6f839c69bc2773d5f2e0e8e6b434db64cf
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-05 08:51:44 +0200

    the kata for checking if an array has a loop another method

--- loopdetector.js
+++ loopdetector.js
@@ -6,3 +6,15 @@ function hasLoop(indices) {
 console.log(hasLoop([1,2,3,4]));
 */
 
+function hasLoop(indices) {
+    const newArr = new Set();
+    let indexHandler = 0;
+    while(indexHandler < indices.length){
+        if(newArr.has(indexHandler)) return true;
+        newArr.add(indexHandler);
+        indexHandler = indices[indexHandler];
+    }
+  return false;
+}
+console.log(hasLoop([1,2,3,4,2]));
+
