f65372af3fa8bcc8650efdb7c526821dc6496513
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-05 08:51:13 +0200

    the kata for checking if an array has a loop

---
+++ loopdetector.js
@@ -0,0 +1,8 @@
+/*
+function hasLoop(indices) {
+  const someone = indices.map((e,index)=> indices.indexOf(e)===index ?true:false);
+  return someone.includes(false)?"true":false;
+}
+console.log(hasLoop([1,2,3,4]));
+*/
+
