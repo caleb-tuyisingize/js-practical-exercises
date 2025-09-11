aaf535d4f291994857128d24deda063f53606b5b
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-11 18:04:29 +0200

    The commit on revising classes

---
+++ toProto.js
@@ -0,0 +1,12 @@
+class Mevis{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+        console.log(`Hello ${this.name} you are ${this.age} years old`);
+    }
+    
+}
+
+const mevis = new Mevis("Caleb", 23);
+
+const user1 = Mevis.mevis;
