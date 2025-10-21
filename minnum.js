c98065e7fa91c392e190a5efd110399e6d130a2e
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-21 19:26:26 +0200

    .

--- minnum.js
+++ minnum.js
@@ -2,6 +2,6 @@ function findShort(s){
 
   return Math.min(...s.split(" ").map(word => word.length));
 }
-console.log(findShort("Simple given a string of words"));
+console.log(findShort("Simple given a string of words and we find shortest one"));
 console.log(findShort("I love JavaScript"));
 console.log(findShort("Coding is amazing"));
