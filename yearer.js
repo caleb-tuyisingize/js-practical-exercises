916d086d84c9d39b373c571ca010cc518226efa2
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-30 09:18:36 +0200

    Logic before lounding up

---
+++ yearer.js
@@ -0,0 +1,8 @@
+function century(year) {
+  return year / 100;
+}
+console.log(century(1705)); // 18
+console.log(century(1900)); // 19
+console.log(century(1601)); // 17
+console.log(century(2000)); // 20
+console.log(century(2742)); // 28
